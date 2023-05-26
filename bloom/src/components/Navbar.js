b898e8809d7e34068af2fbe8b4c8d67d60f5315f
Rose Ginn <andrearginn@gmail.com>
2023-05-25 23:26:33 -0700

    added navbar content, basic page routes

---
+++ bloom/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navbar = () => {
+
+  return (
+  <div classname="navbar">
+    <ul classname="nav">
+      <li><a href="./Profile" id="nav-font">Profile</a></li>
+      <li><a href="./Calculate" id="nav-font">Footprint</a></li>
+      <li><a href="./Learn" id="nav-font">Learn</a></li>
+      <li><a href="./Donate" id="nav-font">Get Involved</a></li>
+    </ul>
+  </div>
+  );
+}
+
+  export default Navbar;
+
+ 
