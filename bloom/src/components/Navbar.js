9df6e21858a0e0da24a4fe81498f78ea6ec87fd5
Rose Ginn <andrearginn@gmail.com>
2023-05-29 13:15:26 -0700

    added beginnings of login/signup/logout features

--- bloom/src/components/Navbar.js
+++ bloom/src/components/Navbar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Signup from "./Signup";
+import Login from "./Login"
 
 const Navbar = () => {
 
@@ -9,6 +11,10 @@ const Navbar = () => {
       <li><a href="./Calculate" id="nav-font">Footprint</a></li>
       <li><a href="./Learn" id="nav-font">Learn</a></li>
       <li><a href="./Donate" id="nav-font">Get Involved</a></li>
+      <li><a href="./Signup" id="nav-font">Sign Up</a></li>
+      <li><a href="./Login" id="nav-font">Log In</a></li>
+      <Signup />
+      <Login />
     </ul>
   </div>
   );
