0ab6162720432b185260017c092bde41d5319b28
bruano95 <bruano@uw.edu>
2023-05-31 18:56:57 -0700

    added preloader

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Home from './pages/Home.js';
+import Preloader from './components/Preloader.js';
 import './App.css';
 
 function App() {
   return (
+    <>
+    <Preloader />
     <Home />
+    </>
   );
 }
 
