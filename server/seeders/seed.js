d6f42e336339292f6d1088174810e8edc1c9047d
Jorgen Baertsch <jorgenpaulmusic@gmail.com>
2023-06-01 19:02:21 -0700

    modal

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -2,3 +2,18 @@ const db = require('../config/connection');
 const {User, Action} = require('../models');
 const actionSeeds = require('./actionSeeds.js');
 const userSeeds = require('./userSeeds.js');
+
+db.once('open', async()=>{
+    try{
+        await Action.deleteMany({});
+        await User.deleteMany({});
+
+        await User.create(userSeeds);
+        await Action.create(actionSeeds);
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    console.log('Seed Complete');
+    process.exit(0)
+})
