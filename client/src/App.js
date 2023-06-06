cf716f98245db5fc446ddb5d3769c5932c13416d
Meljska-Fawn <meli.explores@gmail.com>
2023-06-06 15:11:22 -0700

    added icon and finished text content for donate page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.js';
 import Profile from './pages/Profile.js';
+import Donate from './pages/Donate.js';
 import Preloader from './components/Preloader.js';
 import Navbar from './components/Navbar.js';
 import Learn from './pages/Learn.js';
@@ -46,6 +47,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/Profile" element={<Profile />} />
           <Route path="/Learn" element={<Learn />} />
+          <Route path="/Donate" element={<Donate />} />
         </Routes>
         </>
       </Router>
--- client/src/pages/Donate.js
+++ client/src/pages/Donate.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import DonateForm from '../components/DonateForm';
 import { Link } from 'react-router-dom';
+import FilterVintageRoundedIcon from '@mui/icons-material/FilterVintageRounded';
 
 const Donate = () => {
 
@@ -18,18 +19,18 @@ const Donate = () => {
                         <div>
                             <span>
                                 <p>
-                                    <span><svg></svg></span>
+                                    <span><FilterVintageRoundedIcon /></span>
                                     <span>Protect habitat for wildlife</span>
                                 </p>
                             </span>
                             <span>
                                 <p>
-                                    <span><svg></svg></span>
+                                    <span><FilterVintageRoundedIcon /></span>
                                     <span>Global partnerships for tree planting</span>
                                 </p>
                             </span> <span>
                                 <p>
-                                    <span><svg></svg></span>
+                                    <span><FilterVintageRoundedIcon /></span>
                                     <span>Environmental education and awareness</span>
                                 </p>
                             </span>
@@ -38,28 +39,28 @@ const Donate = () => {
                     </div>
                     <div>
                         <img class="w-full aspect-square" src="http://placehold.it/" alt="" />
-                        <h4>?</h4>
-                        <p>This organization engages in reforestation efforts worldwide. They aim to combat deforestation, restore ecosystems, and create a sustainable future for the planet.</p>
+                        <h4>Coming Clean</h4>
+                        <p>This organization is a nonprofit environmental health collaborative dedicated to transforming the chemical industry into a safe and sustainable source of energy. Their mission is to reduce sources of chemical exposure in people's lives, especially in disproportionately impacted populations, and advocate for corporate and marketplace changes to address toxic chemicals as a public health threat.</p>
                         <div>
                             <span>
                                 <p>
-                                    <span><svg></svg></span>
-                                    <span>Protect habitat for wildlife</span>
+                                    <span><FilterVintageRoundedIcon /></span>
+                                    <span>Advancing chemical policy reform</span>
                                 </p>
                             </span>
                             <span>
                                 <p>
-                                    <span><svg></svg></span>
-                                    <span>Global partnerships for tree planting</span>
+                                    <span><FilterVintageRoundedIcon /></span>
+                                    <span>Promoting environmental health and justice</span>
                                 </p>
                             </span> <span>
                                 <p>
-                                    <span><svg></svg></span>
-                                    <span>Environmental education and awareness</span>
+                                    <span><FilterVintageRoundedIcon /></span>
+                                    <span>Holding corporations accountable for pollution</span>
                                 </p>
                             </span>
                         </div>
-                        <Link as={Link} to='https://onetreeplanted.org/'>Learn more</Link>
+                        <Link as={Link} to='https://www.comingcleaninc.org/'>Learn more</Link>
                     </div>
                     <div>
                         <h4>Carbon180</h4>
@@ -67,18 +68,18 @@ const Donate = () => {
                         <div>
                             <span>
                                 <p>
-                                    <span><svg></svg></span>
+                                    <span><FilterVintageRoundedIcon /></span>
                                     <span>Promotes better climate policy</span>
                                 </p>
                             </span>
                             <span>
                                 <p>
-                                    <span><svg></svg></span>
+                                    <span><FilterVintageRoundedIcon /></span>
                                     <span>Tracks carbon removal legislation</span>
                                 </p>
                             </span> <span>
                                 <p>
-                                    <span><svg></svg></span>
+                                    <span><FilterVintageRoundedIcon /></span>
                                     <span>Promotes carbon-removal on U.S. farmland</span>
                                 </p>
                             </span>
@@ -91,18 +92,18 @@ const Donate = () => {
                         <div>
                             <span>
                                 <p>
-                                    <span><svg></svg></span>
+                                    <span><FilterVintageRoundedIcon /></span>
                                     <span>Influences politicians on climate policy</span>
                                 </p>
                             </span>
                             <span>
                                 <p>
-                                    <span><svg></svg></span>
+                                    <span><FilterVintageRoundedIcon /></span>
                                     <span>Top-rated climate charity</span>
                                 </p>
                             </span> <span>
                                 <p>
-                                    <span><svg></svg></span>
+                                    <span><FilterVintageRoundedIcon /></span>
                                     <span>Promotes key climate solutions</span>
                                 </p>
                             </span>
@@ -115,18 +116,18 @@ const Donate = () => {
                         <div>
                             <span>
                                 <p>
-                                    <span><svg></svg></span>
+                                    <span><FilterVintageRoundedIcon /></span>
                                     <span>Protects biodiversity</span>
                                 </p>
                             </span>
                             <span>
                                 <p>
-                                    <span><svg></svg></span>
+                                    <span><FilterVintageRoundedIcon /></span>
                                     <span>Promotes Indigenous land rights</span>
                                 </p>
                             </span> <span>
                                 <p>
-                                    <span><svg></svg></span>
+                                    <span><FilterVintageRoundedIcon /></span>
                                     <span>Prevents deforestation</span>
                                 </p>
                             </span>
@@ -135,23 +136,24 @@ const Donate = () => {
                     </div>
                     <div>
                         <h4>Climate Ride</h4>
-                        <p>This organization is a policy group that advocates for new technologies and policies needed to get to a zero-emissions, high-energy planet at an affordable cost.</p>
+                        <p>This group organizes charitable multi-day cycling and hiking events to raise funds and awareness for sustainable solutions to climate change. By combining physical challenges with advocacy, Climate Ride empowers participants to support renewable energy, active transportation, and other initiatives that reduce CO2 emissions and promote a sustainable future.</p>
                         <div>
                             <span>
                                 <p>
-                                    <span><svg></svg></span>
-                                    <span>Influences politicians on climate policy</span>
+                                    <span><FilterVintageRoundedIcon /></span>
+                                    <span>Advocacy for renewable energy
+</span>
                                 </p>
                             </span>
                             <span>
                                 <p>
-                                    <span><svg></svg></span>
-                                    <span>Top-rated climate charity</span>
+                                    <span><FilterVintageRoundedIcon /></span>
+                                    <span>Promotes active transporation solutions</span>
                                 </p>
                             </span> <span>
                                 <p>
-                                    <span><svg></svg></span>
-                                    <span>Promotes key climate solutions</span>
+                                    <span><FilterVintageRoundedIcon /></span>
+                                    <span>Supports sustainability through empowering others</span>
                                 </p>
                             </span>
                         </div>
