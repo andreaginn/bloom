003d03be479040631cec91ac9a2f5fb82de1a231
Meljska-Fawn <meli.explores@gmail.com>
2023-06-05 19:00:33 -0700

    moved advice to homepage and made it a component

---
+++ client/src/components/AdviceDisplay.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import reduceFootprints from '../data/reduceFootprints';
+
+const AdviceDisplay = ({ advice }) => {
+    return <p>{advice}</p>;
+};
+
+const Advice = () => {
+    const [displayedAdvice, setDisplayedAdvice] = useState([]);
+
+    // Logic to select random advice
+    const selectRandomAdvice = () => {
+        const randomAdvice = [];
+        const reduceFootprintsCopy = [...reduceFootprints];
+
+        for (let i = 0; i < 10; i++) {
+            const randomIndex = Math.floor(Math.random() * reduceFootprintsCopy.length);
+            randomAdvice.push(reduceFootprintsCopy.splice(randomIndex, 1)[0]);
+        }
+
+        setDisplayedAdvice(randomAdvice);
+    };
+
+    // Call selectRandomAdvice on component mount
+    useEffect(() => {
+        selectRandomAdvice();
+    }, []);
+
+    return (
+        <>
+            <div>
+                <h3>10 Ways to Reduce Your Carbon Footprint</h3>
+                {displayedAdvice.map((advices, index) => (
+                    <AdviceDisplay key={index} advice={advices.advice} />
+                ))}
+            </div>
+        </>
+    );
+};
+
+export default Advice;
