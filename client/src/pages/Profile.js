107d679d832eb3c6726f0a0cd3aaed726ef0a25e
Meljska-Fawn <meli.explores@gmail.com>
2023-06-05 23:23:52 -0700

    added donate.js page and tailwinds

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
-import DonateForm from '../components/DonateForm';
 import ImpactModal from '../components/ImpactModal.js'
 import Button from '../components/Button.js'
 import Chart from 'chart.js/auto'
-import {useQuery} from '@apollo/client'
-import {QUERY_ME} from '../utils/queries.js'
+import { useQuery } from '@apollo/client'
+import { QUERY_ME } from '../utils/queries.js'
 import '../styles/Profile.css'
 import ChartDisplay from '../components/ChartDisplay.js';
-
 
 const Profile = () => {
     const { data, loading } = useQuery(QUERY_ME);
@@ -16,7 +14,7 @@ const Profile = () => {
 
     var userData = data?.me || {};
     console.log(`User Data ${userData}`)
-      
+
     const handleClick = () => {
         console.log('Impact Button Clicked')
         setModalOpen(true)
@@ -25,22 +23,19 @@ const Profile = () => {
     if (loading) {
         return <h2>LOADING...</h2>;
     }
-    
+
     return (
         <div className="home-body">
-            <div className = "impactScoreDisplay">
+            <div className="impactScoreDisplay">
                 <h2>Your Total Impact:</h2>
                 {userData.impactScore} Kg
             </div>
-            <div>
-                <DonateForm />
-            </div>
-             <Button content={"Log Your Impact"} onClick={() => handleClick()} />
+            <Button content={"Log Your Impact"} onClick={() => handleClick()} />
             {modalOpen && <ImpactModal onClose={() => setModalOpen(false)} />}
 
             {userData.dailyImpact[0] &&
-            <ChartDisplay date = {userData.dailyImpact[0].date} travelContribution = {userData.dailyImpact[0].travelContribution} energyContribution = {userData.dailyImpact[0].energyContribution} foodContribution = {userData.dailyImpact[0].foodContribution}  />
-}
+                <ChartDisplay date={userData.dailyImpact[0].date} travelContribution={userData.dailyImpact[0].travelContribution} energyContribution={userData.dailyImpact[0].energyContribution} foodContribution={userData.dailyImpact[0].foodContribution} />
+            }
         </div>
     )
 }
