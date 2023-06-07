80442f439308d7aae497178214d85ff0766c8db9
Meljska-Fawn <meli.explores@gmail.com>
2023-06-07 12:14:36 -0700

    styling donate page

--- client/src/pages/Donate.js
+++ client/src/pages/Donate.js
@@ -11,22 +11,23 @@ const Donate = () => {
                 <div className="col-start-1 col-end-3 mt-5">
                     <DonateForm />
                 </div>
-                <div className="col-start-3 col-end-7 place-self-center text-7xl font-bold text-slate-700 mt-3" style={{ display: 'inline-block' }}>
-                    <span className="text-orange-400">Bloom</span>has choosen these planet-saving projects to support.
+                <div className="col-start-3 col-end-7 place-self-center text-7xl font-bold text-slate-700 mt-3 mr-3" style={{ display: 'inline-block' }}>
+                    <span className="text-orange-400">Offset</span>your carbon footprint by supporting projects you care about.
                 </div>
-                <div className="col-start-3 col-end-7 mt-5">
+
+                <div className="col-start-2 col-end-7 mt-5">
                     <img className="w-full" src="./images/one-tree-planted.webp" alt="" />
                     <div className="mt-4 text-5xl font-bold text-slate-700 leading-snug">One Tree Planted</div>
                     <p className="mt-2 mb-3 text-xl text-slate-600">This organization engages in reforestation efforts worldwide. They aim to combat deforestation, restore ecosystems, and create a sustainable future for the planet.</p>
                 </div>
-                <div className="col-start-3 col-end-5 mb-3">
+                <div className="col-start-2 col-end-5 mb-3">
                     <div>
-                        <span><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Protect habitat for wildlife</div></span>
-                        <span><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Global partnerships for tree planting</div></span>
-                        <span><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Environmental education and awareness</div></span>
+                        <span className="pb-2"><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Protect habitat for wildlife</div></span>
+                        <span className="pb-2"><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Global partnerships for tree planting</div></span>
+                        <span className="pb-2"><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Environmental education and awareness</div></span>
                     </div>
                 </div>
-                <div className="col-start-5 col-end-7 place-self-start">
+                <div className="col-start-5 col-end-6 place-self-start">
                     <Link className="btn-primary" as={Link} to='https://onetreeplanted.org/'>Learn more</Link>
                 </div>
             </div>
@@ -53,70 +54,59 @@ const Donate = () => {
                 <div className="col-start-1 col-end-5">
                     <img className="w-full" src="./images/carbon180.jpeg" alt="" />
                 </div>
-                <div className="col-start-5 col-end-7">
-                    <div className="pl-8 mb-4 text-5xl font-bold text-slate-700 leading-snug">Carbon180</div>
-                    <div className="text-xl text-slate-600">
+                <div className="col-start-5 col-end-7 ">
+                    <div className="mb-3 text-5xl font-bold text-slate-700 leading-snug">Carbon180</div>
+                    <div className="text-xl text-slate-600 pb-3 pr-2">
                         <p>This organization helps improve the transparency and scientific integrity of carbon removal and climate solutions through open data, tools, and leadership.</p>
                     </div>
                     <div>
-                        <span><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Promotes better climate policy</div></span>
-                        <span><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Tracks carbon removal legislation</div></span>
-                        <span><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Promotes carbon-removal on U.S. farmland</div></span>
+                        <span className="pb-2"><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Promotes better climate policy</div></span>
+                        <span className="pb-2"><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Tracks carbon removal legislation</div></span>
+                        <span className="pb-2"><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Promotes carbon-removal on U.S. farmland</div></span>
                     </div>
-                    <div className="col-start-5 col-end-7">
+                    <div className="col-start-5 col-end-7 mt-3">
                         <Link className="btn-primary" as={Link} to='https://carbon180.org/'>Learn more</Link>
                     </div>
                 </div>
             </div>
-            <div>
-                <img className="w-full" src="./images/CATF.jpeg" alt="" />
-                <h4>Clean Air Task Force</h4>
-                <p>This organization is a policy group that advocates for new technologies and policies needed to get to a zero-emissions, high-energy planet at an affordable cost.</p>
-                <div>
-                    <span>
-                        <p>
-                            <span><FilterVintageRoundedIcon /></span>
-                            <span>Influences politicians on climate policy</span>
-                        </p>
-                    </span>
-                    <span>
-                        <p>
-                            <span><FilterVintageRoundedIcon /></span>
-                            <span>Top-rated climate charity</span>
-                        </p>
-                    </span> <span>
-                        <p>
-                            <span><FilterVintageRoundedIcon /></span>
-                            <span>Promotes key climate solutions</span>
-                        </p>
-                    </span>
+
+            <div className="grid grid-cols-6 gap-4 mt-5">
+                <div className="col-start-2 col-end-7 mt-5">
+                    <img className="w-full" src="./images/CATF.jpeg" alt="" />
+                    <div className="mt-3 text-5xl font-bold text-slate-700 leading-snug">Clean Air Task Force</div>
                 </div>
-                <Link className="impactButton" as={Link} to='https://www.catf.us/'>Learn more</Link>
+                <div className="col-start-2 col-end-6 text-xl text-slate-600">
+                    <p>This organization is a policy group that advocates for new technologies and policies needed to get to a zero-emissions, high-energy planet at an affordable cost.</p>
+                </div>
+                <div className="col-start-2 col-end-5 mb-3">
+                    <div>
+                        <span className="pb-2"><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Influences politicians on climate policy</div></span>
+                        <span className="pb-2"><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Top-rated climate charity</div></span>
+                        <span className="pb-2"><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Promotes key climate solutions</div></span>
+                    </div>
+                </div>
+                <div className="col-start-5 col-end-6 place-self-start">
+                    <Link className="btn-primary" as={Link} to='https://www.catf.us/'>Learn more</Link>
+                </div>
+
             </div>
-            <div>
-                <img className="w-full" src="./images/rainforest800x450.jpeg" alt="" />
-                <h4>Rainforest Foundation</h4>
-                <p>This project gives Indigenous Amazonians the tools they need to protect their rainforest home. Using satellite monitoring and drones, Indigenous monitors detect deforestation early. This allows communities to prevent deforestation before it reaches a large scale.</p>
+
+            <div className="grid grid-cols-6 gap-4 mt-5">
                 <div>
-                    <span>
-                        <p>
-                            <span><FilterVintageRoundedIcon /></span>
-                            <span>Protects biodiversity</span>
-                        </p>
-                    </span>
-                    <span>
-                        <p>
-                            <span><FilterVintageRoundedIcon /></span>
-                            <span>Promotes Indigenous land rights</span>
-                        </p>
-                    </span> <span>
-                        <p>
-                            <span><FilterVintageRoundedIcon /></span>
-                            <span>Prevents deforestation</span>
-                        </p>
-                    </span>
+                    <img className="w-full" src="./images/rainforest800x450.jpeg" alt="" />
+                    <h4>Rainforest Foundation</h4>
+                    <p>This project gives Indigenous Amazonians the tools they need to protect their rainforest home. Using satellite monitoring and drones, Indigenous monitors detect deforestation early. This allows communities to prevent deforestation before it reaches a large scale.</p>
+                    <div>
+
+                        <span><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Protects biodiversity</div></span>
+
+                        <span><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Promotes Indigenous land rights</div></span>
+
+                        <span><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Prevents deforestation</div></span>
+
+                    </div>
+                    <Link className="impactButton" as={Link} to='https://rainforestfoundation.org/'>Learn more</Link>
                 </div>
-                <Link className="impactButton" as={Link} to='https://rainforestfoundation.org/'>Learn more</Link>
             </div>
             <div>
                 <img className="w-full" src="./images/Climate-Ride1024x683.jpeg" alt="" />
