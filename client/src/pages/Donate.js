b59c98a2a06762703194d7483294696a677eacbb
Meljska-Fawn <meli.explores@gmail.com>
2023-06-06 23:12:22 -0700

    styled donate page more

--- client/src/pages/Donate.js
+++ client/src/pages/Donate.js
@@ -11,71 +11,62 @@ const Donate = () => {
                 <div className="col-start-1 col-end-3 mt-5">
                     <DonateForm />
                 </div>
-                <div className="col-start-3 col-end-7">
+                <div className="col-start-3 col-end-7 place-self-center text-7xl font-bold text-slate-700 mt-3" style={{ display: 'inline-block' }}>
+                    <span className="text-orange-400">Bloom</span>has choosen these planet-saving projects to support.
+                </div>
+                <div className="col-start-3 col-end-7 mt-5">
                     <img className="w-full" src="./images/one-tree-planted.webp" alt="" />
                     <div className="mt-4 text-5xl font-bold text-slate-700 leading-snug">One Tree Planted</div>
                     <p className="mt-2 mb-3 text-xl text-slate-600">This organization engages in reforestation efforts worldwide. They aim to combat deforestation, restore ecosystems, and create a sustainable future for the planet.</p>
                 </div>
-                <div className="mb-3">
-                    <div className="col-start-3 col-end-6">
+                <div className="col-start-3 col-end-5 mb-3">
+                    <div>
                         <span><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Protect habitat for wildlife</div></span>
                         <span><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Global partnerships for tree planting</div></span>
                         <span><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Environmental education and awareness</div></span>
                     </div>
                 </div>
-                <div className="mt-3">
+                <div className="col-start-5 col-end-7 place-self-start">
                     <Link className="btn-primary" as={Link} to='https://onetreeplanted.org/'>Learn more</Link>
                 </div>
             </div>
-            <div>
-                <img className="w-full" src="./images/coming-clean.jpg" alt="" />
-                <h4>Coming Clean</h4>
-                <p>This organization is a nonprofit environmental health collaborative dedicated to transforming the chemical industry into a safe and sustainable source of energy. Their mission is to reduce sources of chemical exposure in people's lives, especially in disproportionately impacted populations, and advocate for corporate and marketplace changes to address toxic chemicals as a public health threat.</p>
-                <div>
-                    <span>
-                        <p>
-                            <span><FilterVintageRoundedIcon /></span>
-                            <span>Advancing chemical policy reform</span>
-                        </p>
-                    </span>
-                    <span>
-                        <p>
-                            <span><FilterVintageRoundedIcon /></span>
-                            <span>Promoting environmental health and justice</span>
-                        </p>
-                    </span> <span>
-                        <p>
-                            <span><FilterVintageRoundedIcon /></span>
-                            <span>Holding corporations accountable for pollution</span>
-                        </p>
-                    </span>
+
+            <div className="grid grid-cols-6 gap-4 mt-5">
+                <div className="col-start-1 col-end-7">
+                    <img className="w-full" src="./images/coming-clean.jpg" alt="" />
                 </div>
-                <Link className="impactButton" as={Link} to='https://www.comingcleaninc.org/'>Learn more</Link>
+                <div className="col-start-1 col-end-3">
+                    <div className="pl-8 pb-8 text-5xl font-bold text-slate-700 leading-snug">Coming Clean</div>
+                    <Link className="btn-primary ml-9" as={Link} to='https://www.comingcleaninc.org/'>Learn more</Link>
+                </div>
+                <div className="col-start-3 col-end-6 text-xl text-slate-600">
+                    <p>This organization is a nonprofit environmental health collaborative dedicated to transforming the chemical industry into a safe and sustainable source of energy. Their mission is to reduce sources of chemical exposure in people's lives, especially in disproportionately impacted populations, and advocate for corporate and marketplace changes to address toxic chemicals as a public health threat.</p>
+                </div>
+                <div className="col-start-3 col-end-6 mb-3">
+                    <span className="pb-2"><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Advancing chemical policy reform</div></span>
+                    <span className="pb-2"><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Promoting environmental health and justice</div></span>
+                    <span><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Holding corporations accountable for pollution</div></span>
+                </div>
             </div>
-            <div>
-                <img className="w-full" src="./images/carbon180.jpeg" alt="" />
-                <h4>Carbon180</h4>
-                <p>This organization helps improve the transparency and scientific integrity of carbon removal and climate solutions through open data, tools, and leadership.</p>
-                <div>
-                    <span>
-                        <p>
-                            <span><FilterVintageRoundedIcon /></span>
-                            <span>Promotes better climate policy</span>
-                        </p>
-                    </span>
-                    <span>
-                        <p>
-                            <span><FilterVintageRoundedIcon /></span>
-                            <span>Tracks carbon removal legislation</span>
-                        </p>
-                    </span> <span>
-                        <p>
-                            <span><FilterVintageRoundedIcon /></span>
-                            <span>Promotes carbon-removal on U.S. farmland</span>
-                        </p>
-                    </span>
+
+            <div className="grid grid-cols-6 gap-4 mt-5">
+                <div className="col-start-1 col-end-5">
+                    <img className="w-full" src="./images/carbon180.jpeg" alt="" />
                 </div>
-                <Link className="impactButton" as={Link} to='https://carbon180.org/'>Learn more</Link>
+                <div className="col-start-5 col-end-7">
+                    <div className="pl-8 mb-4 text-5xl font-bold text-slate-700 leading-snug">Carbon180</div>
+                    <div className="text-xl text-slate-600">
+                        <p>This organization helps improve the transparency and scientific integrity of carbon removal and climate solutions through open data, tools, and leadership.</p>
+                    </div>
+                    <div>
+                        <span><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Promotes better climate policy</div></span>
+                        <span><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Tracks carbon removal legislation</div></span>
+                        <span><FilterVintageRoundedIcon /><div className="pl-2 text-xl text-slate-600">Promotes carbon-removal on U.S. farmland</div></span>
+                    </div>
+                    <div className="col-start-5 col-end-7">
+                        <Link className="btn-primary" as={Link} to='https://carbon180.org/'>Learn more</Link>
+                    </div>
+                </div>
             </div>
             <div>
                 <img className="w-full" src="./images/CATF.jpeg" alt="" />
