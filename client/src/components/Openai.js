ef1213ec3e2c5959d3e282577ef1ce37c9099c9e
bruano95 <bruano@uw.edu>
2023-06-11 15:21:46 -0700

    updates to openai api

--- client/src/components/Openai.js
+++ client/src/components/Openai.js
@@ -1,74 +1,96 @@
-import { useState } from 'react';
-import axios from 'axios';
-// import dotenv from 'dotenv';
-// dotenv.config();
+import { useState } from 'react'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-require('dotenv').config()
+const systemMessage = {
+  "role": "system", "content": "Explain things like you're talking to someone with 2 years of climate change experience."
+}
 
 function Openai() {
-  const[cbResponse, setCbResponse] = useState('')
-  const [userInput, setUserInput] = useState('')
-  const [isLoading, setIsLoading] = useState(false)
+  const [messages, setMessages] = useState([
+    {
+      message: "I'm Bloom GPT! Ask anything to learn more about climate change or carbon emissions!",
+      sentTime: "just now",
+      sender: "BloomGPT"
+    }
+  ]);
+  const [isTyping, setIsTyping] = useState(false);
 
-  const handleSendData = async(event) => {
-    event.preventDefault()
-    const url = 'https://api.openai.com/v1/chat/completions';
-    const headers = {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_KEY}`
+  const handleSend = async (message) => {
+    const newMessage = {
+      message,
+      direction: 'outgoing',
+      sender: "user"
     };
 
-    const data = {
-      model: "gpt-3.5-turbo-0301",
-      messages: [{"role": "user", "content": userInput}]
-    };
+    const newMessages = [...messages, newMessage];
+    
+    setMessages(newMessages);
 
-    setIsLoading(true);
+    setIsTyping(true);
+    await processMessageToChatGPT(newMessages);
+  };
 
-    axios.post(url, data, {headers:headers}).then((response) => {
-      console.log(response);
-      setUserInput((preUserInput) => [...preUserInput, { type: 'bot', message: response.data.choise[0].message.content}]);
-      setIsLoading(false);
-    }).catch((error) => {
-      setIsLoading(false);
-      console.log(error)
-    })
+  async function processMessageToChatGPT(chatMessages) {
+
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if (messageObject.sender === "ChatGPT") {
+        role = "assistant";
+      } else {
+        role = "user";
+      }
+      return { role: role, content: messageObject.message}
+    });
+
+    const apiRequestBody = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        systemMessage,
+        ...apiMessages
+      ]
+    }
+
+    await fetch("https://api.openai.com/v1/chat/completions", 
+    {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + process.env.REACT_APP_OPENAI_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(apiRequestBody)
+    }).then((data) => {
+      return data.json();
+    }).then((data) => {
+      console.log(data);
+      setMessages([...chatMessages, {
+        message: data.choices[0].message.content,
+        sender: "BloomGPT"
+      }]);
+      setIsTyping(false);
+    });
   }
 
-
   return (
-    <div className="container mx-auto max-w-[700px]">
-      <div className="flex flex-col h-screen bg-gray-900">
-        <h1 className="bg-gradient-to-r from-green-500 to-green-500 text-transparent bg-clip-text text-center py-3 font-bold text-6xl">ChatGPT</h1>
-        <div className="flex-grow p-6">
-          <div className="flex flex-col space-y-4">
-          <div key={userInput} className={`flex ${
-            userInput.type === 'user' ? 'justify-end' : 'justify-start'
-            }`}>
-            <div className={`${
-              userInput.type === 'user' ? 'bg-purple-500' : 'bg-gray-800'
-            } rounded-lg p-4 text-white max-w-sm`}>
-            {userInput.message}
-            </div>
-            </div>
-            {
-              isLoading &&
-              <div key={cbResponse.length} className="flex justify-start">
-                  <div className="bg-gray-800 rounded-lg p-4 text-white max-w-sm">
-                  </div>
-              </div>
-            }
+    <div className="App">
+      <div style={{ position:"relative", height: "800px", width: "700px"  }}>
+        <MainContainer>
+          <ChatContainer>       
+            <MessageList 
+              scrollBehavior="smooth" 
+              typingIndicator={isTyping ? <TypingIndicator content="Bloom GPT is typing" /> : null}
+            >
+              {messages.map((message, i) => {
+                console.log(message)
+                return <Message key={i} model={message} />
+              })}
+            </MessageList>
+            <MessageInput placeholder="Type message here" onSend={handleSend} />        
+          </ChatContainer>
+        </MainContainer>
       </div>
-        </div>
-        <form onSubmit={handleSendData} className="flex-none p-6">
-          <div className="flex rounded-lg border border-gray-700 bg-gray-800">  
-        <input type="text" className="flex-grow px-4 py-2 bg-transparent text-white focus:outline-none" placeholder="Type your message..." value={userInput} onChange={(e) => setUserInput(e.target.value)} />
-            <button type="submit" className="bg-purple-500 rounded-lg px-4 py-2 text-white font-semibold focus:outline-none hover:bg-purple-600 transition-colors duration-300">Send</button>
-            </div>
-        </form>
-        </div>
     </div>
   )
 }
 
-export default Openai;
+export default Openai
