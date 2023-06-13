b28a2ccff6c975fc6e44bea421dcc52e9328d519
Meljska-Fawn <meli.explores@gmail.com>
2023-06-12 20:33:21 -0700

    AQI styled

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Lottie from 'react-lottie';
 import animationData from '../fjO3sek7ZT.json';
-import Signup from "./Signup";
-import Login from "./Login";
 import Auth from '../utils/auth';
 import Button from '../components/Button'
 import ImpactModal from './ImpactModal';
@@ -36,13 +34,13 @@ const Navbar = ({openModal}) => {
           </div>
         </span>oom</h1>
       </Link>
-      <ul className="nav-1">
+      <ul className="navbar-nav">
         {Auth.loggedIn() ? (
           <>
-            <li><Link as={Link} to='/Profile' id="nav-font">Profile</Link></li>
-            <li><Link as={Link} to='/Donate' id="nav-font">Donate</Link></li>
-            <li><Link as={Link} to='/Learn' id="nav-font">Learn</Link></li>
-            <Link id="nav-font" onClick={Auth.logout}>Logout</Link>
+            <li><Link as={Link} to='/Profile'>Profile</Link></li>
+            <li><Link as={Link} to='/Donate'>Donate</Link></li>
+            <li><Link as={Link} to='/Learn'>Learn</Link></li>
+            <Link onClick={Auth.logout}>Logout</Link>
             {/* <Button content={"Log Your Impact"} onClick={() => handleClick()} />
             {modalOpen && <ImpactModal onClose={() => setModalOpen(false)} />} */}
             <Button content = {"Log Your Impact" }onClick={openModal}/>
