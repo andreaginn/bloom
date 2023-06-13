1daf33bb65375a8bef2bd7a99dd78bf22449dd33
Jorgen Baertsch <jorgenpaulmusic@gmail.com>
2023-06-12 22:18:53 -0500

    Navbar and Advice

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,16 +2,16 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Lottie from 'react-lottie';
 import animationData from '../fjO3sek7ZT.json';
-import Signup from "./Signup";
-import Login from "./Login";
 import Auth from '../utils/auth';
 import Button from '../components/Button'
-import ImpactModal from './ImpactModal';
+import {Menu, MenuItem, useMediaQuery} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import '../styles/Button.css'
 
 const Navbar = ({openModal}) => {
 
   const [modalOpen, setModalOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const defaultOptions = {
     loop: false,
@@ -22,20 +22,74 @@ const Navbar = ({openModal}) => {
     }
   };
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleMenuClose = () => {
+    setMenuOpen(false);
+  };
+
   const handleClick = () => {
     console.log('Impact Button Clicked')
     setModalOpen(true)
 }
 
+const isMobile = useMediaQuery('(max-width: 768px)'); // Adjust the breakpoint as needed
+
   return (
     <div className="navbar">
       <Link className="logo" as={Link} to='/' >
-        <h1 className="logo-text">B<span>
+        
           <div className="animation-container">
             <Lottie options={defaultOptions} />
           </div>
-        </span>oom</h1>
+      
       </Link>
+      {isMobile ? (
+        <>
+      
+          <MenuIcon  sx = {{fontSize: 50}} className = "menu-icon" onClick={toggleMenu} />
+          <Menu
+            anchorEl={document.querySelector('.menu-icon')}
+            open={menuOpen}
+            onClose={handleMenuClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+          >
+            {Auth.loggedIn() ? (
+              <>
+                <MenuItem onClick={handleMenuClose}>
+                  <Link as={Link} to="/Profile" id="nav-font">
+                    Profile
+                  </Link>
+                </MenuItem>
+                <MenuItem onClick={handleMenuClose}>
+                  <Link as={Link} to="/Donate" id="nav-font">
+                    Donate
+                  </Link>
+                </MenuItem>
+                <MenuItem onClick={handleMenuClose}>
+                  <Link as={Link} to="/Learn" id="nav-font">
+                    Learn
+                  </Link>
+                </MenuItem>
+                <MenuItem onClick={() => { Auth.logout(); handleMenuClose(); }}>
+                  Logout
+                </MenuItem>
+                <MenuItem className = "modalMenuItem" onClick={() => { openModal(); handleMenuClose(); }}>
+                  <p className = "modalMenuItem">Log Your Impact</p>
+                </MenuItem>
+                
+              </>
+            ) : (
+              <>
+              </>
+            )}
+          </Menu>
+        </>
+      ) : (
       <ul className="nav-1">
         {Auth.loggedIn() ? (
           <>
@@ -43,16 +97,14 @@ const Navbar = ({openModal}) => {
             <li><Link as={Link} to='/Donate' id="nav-font">Donate</Link></li>
             <li><Link as={Link} to='/Learn' id="nav-font">Learn</Link></li>
             <Link id="nav-font" onClick={Auth.logout}>Logout</Link>
-            {/* <Button content={"Log Your Impact"} onClick={() => handleClick()} />
-            {modalOpen && <ImpactModal onClose={() => setModalOpen(false)} />} */}
-            <Button content = {"Log Your Impact" }onClick={openModal}/>
+            <Button content = {"Log Your Impact" } onClick={openModal}/>
           </>
-        ) : (
+        ): (
           <>
           </>
         )}
       </ul>
-      
+      )}
     </div>
   );
 }
